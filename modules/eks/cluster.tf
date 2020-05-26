b931bd2a7437496272ce02845bb8d6f49a7104c6
Özgür <ozgur.v.amac@derinworksllc.com>
2020-05-25 21:27:55 -0400

    eks  add kubernetes provider back in and output cluster id and worker iam role to use for input to other dependent modules

--- modules/eks/cluster.tf
+++ modules/eks/cluster.tf
@@ -1,3 +1,24 @@
+data "aws_region" "current" {}
+
+data "aws_availability_zones" "available" {}
+
+data "aws_eks_cluster" "main" {
+  name = module.aws-eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = module.aws-eks.cluster_id
+}
+
+provider "kubernetes" {
+  alias                  = "eks"
+  host                   = data.aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.main.token
+  load_config_file       = false
+  version                = ">= 1.11.1"
+}
+
 module "aws-eks" {
   source       = "terraform-aws-modules/eks/aws"
   version      = "11.1.0"
--- modules/eks/providers.tf
+++
@@ -1,8 +0,0 @@
-provider "kubernetes" {
-  alias                  = "eks"
-  host                   = data.aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.main.token
-  load_config_file       = false
-  version                = ">= 1.11.1"
-}
